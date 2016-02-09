5ec8d9c2ea7fcd938be6a0ddc15a70c334f337e7
Mathieu Dutour <mathieu@dutour.me>
2016-02-09 10:42:17 +0000

    add blacklisted keys as the third argument of filter

--- src/decorators/__tests__/filter-test.js
+++ src/decorators/__tests__/filter-test.js
@@ -83,4 +83,76 @@ describe('decorators/filter', () => {
 
         save.should.have.been.calledWith({ a: { first: null } });
     });
+
+    it('should have a empty blacklist by default', () => {
+        const save = sinon.spy();
+        const engine = filter({ save }, ['key']);
+
+        engine.save({ key: 'value' });
+
+        save.should.have.been.calledWith({ key: 'value' });
+    });
+
+    it('should not save blacklisted keys', () => {
+        const save = sinon.spy();
+        const engine = filter({ save }, ['ignored'], ['ignored']);
+
+        engine.save({ key: 'value', ignored: true });
+
+        save.should.have.been.calledWith({});
+    });
+
+    it('should not save blacklisted keys paths', () => {
+        const save = sinon.spy();
+        const engine = filter({ save }, ['key', 'some'], [['some', 'ignored']]);
+
+        engine.save({ key: 'value', some: { ignored: true } });
+
+        save.should.have.been.calledWith({ key: 'value', some: {} });
+    });
+
+    it('should ignore not existing but blacklisted keys', () => {
+        const save = sinon.spy();
+        const engine = filter({ save }, ['key'], ['ignored']);
+
+        engine.save({ key: 'value' });
+
+        save.should.have.been.calledWith({ key: 'value' });
+    });
+
+    it('should ignore not existing but blacklisted key paths', () => {
+        const save = sinon.spy();
+        const engine = filter({ save }, ['key'], [['some', 'ignored']]);
+
+        engine.save({ key: 'value' });
+
+        save.should.have.been.calledWith({ key: 'value' });
+    });
+
+    it('should support immutable', () => {
+        const save = sinon.spy();
+        const engine = filter({ save }, [['some', 'key']], [['some', 'key']]);
+
+        engine.save({ some: map({ key: 42 }) });
+
+        save.should.have.been.calledWith({ some: {} });
+    });
+
+    it('should handle null values (PR #64)', () => {
+        const save = sinon.spy();
+        const engine = filter({ save }, [['a', 'first']], [['a', 'first']]);
+
+        engine.save({ a: { first: null, second: 2 } });
+
+        save.should.have.been.calledWith({ a: {} });
+    });
+
+    it('should handle null values (PR #64) - immutable', () => {
+        const save = sinon.spy();
+        const engine = filter({ save }, [['a', 'first']], [['a', 'first']]);
+
+        engine.save(map({ a: { first: null, second: 2 } }));
+
+        save.should.have.been.calledWith({ a: {} });
+    });
 });
